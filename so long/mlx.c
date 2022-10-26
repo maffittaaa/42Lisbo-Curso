616e280b0b726fa7244838e6cedc6a6e97f5b65e
Mafalda Peixeiro antunes marques martins <mpeixeir@c1r18s5.42lisboa.com>
2022-10-26 17:33:04 +0100

    rectangles in mlx

--- so long/mlx.c
+++ so long/mlx.c
@@ -10,28 +10,145 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <X11/X.h>
+#include <X11/keysym.h>
 #include <mlx.h>
+
+# define WINDOW_WIDTH 600
+# define WINDOW_HEIGHT 300
+
+# define MLX_ERROR 1
+
+# define RED_PIXEL 0xFF0000
+# define GREEN_PIXEL 0xFF00
+# define WHITE_PIXEL 0xFFFFFF
 
 typedef struct s_data
 {
-    void    *img;
+    void    *mlx_ptr;
+    void    *win_ptr;
+    t_img   img;
+}   t_data;
+
+typedef struct s_rect
+{
+    int x;
+    int y;
+    int width;
+    int height;
+    int color;
+}   t_rect;
+
+typedef struct s_img
+{
+    void    *mlx_img;
     char    *addr;
-    int     bits_per_panel;
-    int     line_lenght;
+    int     bpp; // bits per pixel
+    int     line_len;
     int     endian;
-}           t_data;
+}   t_img;
+
+//the x and the y coordinates of the rectangle corresponds to its upper left corner
+
+int render_rect(t_data *data, t_rect rect)
+{
+    int i;
+    int j;
+
+    if (data->win_ptr == NULL)
+        return (1);
+    i = rect.y;
+    while (i < rect.y + rect.height)
+    {
+        j = rect.x;
+        while (j < rect.x + rect.width)
+            mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
+        i++;
+    }
+    return (0);
+}
+
+void    render_background(t_data *data, int color)
+{
+    int i;
+    int j;
+
+    if (data->win_ptr == NULL)
+        return ;
+    i = 0;
+    while (i < WINDOW_HEIGHT)
+    {
+        j = 0;
+        while (j < WINDOW_WIDTH)
+            mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, color);
+        i++;
+
+    }
+}
+
+int handle_keypress(int keysym, t_data *data)
+{
+    if (keysym == XK_Escape)
+    {
+        mlx_destroy_window (data->mlx_ptr, data->win_ptr);
+        data->win_ptr = NULL;
+    }
+
+    printf("Keypress: %d\n", keysym);
+    return (0);
+}
+
+int render(t_data *data)
+{
+    render_background(data,WHITE_PIXEL);
+    render_rect(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100,
+                               100, 100, GREEN_PIXEL});
+    render_rect(data, (t_rect){0, 0, 100, 100, RED_PIXEL});
+
+    return (0);
+}
+
+//int handle_keyrelease(int keysym, void *data)
+//{
+//    if (!data)
+//        return (0);
+//    printf("Keyrelease: %d\n", keysym);
+//    return (0);
+//}
+
+//int handle_no_event (void *data)
+//{
+    //esta funcao precisa de existir mas e useless para ja
+//    if (!data)
+//        return (0);
+//    return (0);
+//}
 
 int main(void)
 {
-    void    *mlx;
-    void    *mlx_win;
-    t_data  img;
+    t_data  data;
 
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World!");
-    img.img = mlx_new_image(mlx, 1920, 1080);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_panel, &img.line_lenght, &img.endian);
-    mlx_loop(mlx);
+    data.mlx_ptr= mlx_init();
+    if (data.mlx_ptr == NULL)
+        return (MLX_ERROR);
+    data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
+    if (data.win_ptr == NULL)
+    {
+        free(data.win_ptr);
+        return (MLX_ERROR);
+    }
+    //setup hooks
+    data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+    mlx_loop_hook(data.mlx_ptr, &render, &data);
+    mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data); //ADDED
+    //mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data); // CHANGED
 
+    mlx_loop(data.mlx_ptr);
+
+    //we will exit the loop if there's no window left, and execute this code
+    mlx_destroy_display(data.mlx_ptr);
+    free(data.mlx_ptr);
 }
-
