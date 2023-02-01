f49664f8c90babb556aba25c4286c7e092edba72
Mafalda Peixeiro antunes marques martins <mpeixeir@c2r1s2.42lisboa.com>
2023-02-01 16:27:18 +0000

    .

--- so long/put_image.c
+++ so long/put_image.c
@@ -14,25 +14,23 @@
 
 int render(t_data *data)
 {
-	t_img img;
 
-	img.position.x = 0;
-	img.position.y = 0;
+	data->img_list->position.x = 1;
+	data->img_list->position.y = 1;
 
     if (data->win_ptr == NULL)
         return (1);
-    mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img_list->mlx_img, img.position.x, img.position.y);
-	if(img.position.x == 0)
+    mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img_list->mlx_img, data->img_list->position.x, data->img_list->position.y);
+	if(data->img_list->position.x == 0)
 		return (0);
 	return(0);
 }
 
 int handle_keypress(int keysym, t_data *data)
 {
-	t_img img;
 
-	img.position.x = 0;
-	img.position.y = 0;
+	data->img_list->position.x = 1;
+	data->img_list->position.y = 1;
 
 	if (keysym == XK_Escape)
 	{
@@ -41,14 +39,14 @@ int handle_keypress(int keysym, t_data *data)
 		exit(0);
 	}
 
-	if (keysym == right && img.position.x < WINDOW_WIDTH - data->img_list->width)
-		img.position.x += 25;
-	if (keysym == left && img.position.x > 0)
-		img.position.x -= 25;
-	if (keysym == down && img.position.y < WINDOW_HEIGHT - data->img_list->height)
-		img.position.y += 25;
-	if (keysym == up && img.position.y > 0)
-		img.position.y -= 25;
+	if (keysym == right && data->img_list->position.x < WINDOW_WIDTH - data->img_list->width)
+		data->img_list->position.x += 25;
+	if (keysym == left && data->img_list->position.x > 0)
+		data->img_list->position.x -= 25;
+	if (keysym == down && data->img_list->position.y < WINDOW_HEIGHT - data->img_list->height)
+		data->img_list->position.y += 25;
+	if (keysym == up && data->img_list->position.y > 0)
+		data->img_list->position.y -= 25;
 	printf("Keypress: %d\n", keysym);
 	return (0);
 }
@@ -60,7 +58,7 @@ int main(void)
 	t_data	data;
 	int		i;
 
-	data.img_list->img1 = ft_strdup("images/wall1.xpm");
+	data.img_list->img1 = ft_strdup("images/floor.xpm");
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
 		return (MLX_ERROR);
@@ -71,19 +69,18 @@ int main(void)
 		free(data.win_ptr);
 		return (MLX_ERROR);
 	}
-
 	data.img_list = malloc((5) * sizeof(t_img));
 	i = 0;
 	while(i < 5)
 	{
-		data.img_list[i].position.x = 0;
-		data.img_list[i].position.y = 0;
+		data.img_list[i].position.x = 1;
+		data.img_list[i].position.y = 1;
 		data.img_list[i].width = 200;
 		data.img_list[i].height = 200;
 		data.img_list[i].mlx_img = mlx_new_image(data.mlx_ptr, data.img_list[i].width, data.img_list[i].height);
 		data.img_list[i].addr = mlx_get_data_addr(&(data.img_list[i].mlx_img), &data.img_list[i].bpp, &data.img_list[i].line_len, &data.img_list[i].endian);
-		mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img_list[i].mlx_img, data.img_list[i].position.x, data.img_list[i].position.y);
 		mlx_xpm_file_to_image(data.mlx_ptr, data.img_list[i].img1, &data.img_list[i].width, &data.img_list[i].height);
+		mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img_list[i].mlx_img, data.img_list[i].position.x, data.img_list[i].position.y);
 		//mlx_destroy_image(data.mlx_ptr, data.img_list[i].mlx_img);
 		i++;
 	}
