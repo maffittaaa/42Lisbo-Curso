636c06e3956e82cbec5b1ec905e3b4f2fb406c9a
Mafalda Peixeiro antunes marques martins <mpeixeir@c2r2s4.42lisboa.com>
2023-02-07 17:49:01 +0000

    modified a few things on the error_checker

--- push_swap/error_checker.c
+++ push_swap/error_checker.c
@@ -3,10 +3,16 @@
 int error_checker(t_args *args)
 {
     int i;
+    int base;
 
     i = 0;
+    base = 0;
+
     if (arg[i] > INT_MAX || arg[i] < INT_MIN)
         printf("Error\n");
     if (arg[i] != ft_isdigit(i))
-        printf("Error\n")
+        printf("Error\n");
+    if (!arg[i])
+        prinf("Error\n")
+    
 }
