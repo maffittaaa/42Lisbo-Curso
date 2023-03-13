01cae742e3135425678efa8ce02dcf4807b49a40
Mafalda Peixeiro antunes marques martins <mpeixeir@c2r1s6.42lisboa.com>
2023-03-13 16:08:21 +0000

    ..

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -2,18 +2,6 @@
 
 int main(int argc, char *argv[])
 {
-    // int a;
-    // int b;
-    // int c;
-    // int i;
-
-    // a = ft_atol(argv[1]);
-    // b = ft_atol(argv[2]);
-    // c = ft_atol(argv[3]);
-    // i = 1;
-
-
     error_checker(argc,argv);
-
     return 0;
 }
