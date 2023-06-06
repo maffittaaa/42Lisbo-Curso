b773a4aa4c18e7f819158d8d7dbd1285ad0c6b30
maffittaaa <mafaldamartins2001@gmail.com>
2023-06-06 18:18:46 +0100

    iniciating a new project:fractol, main objective: creating a fractal

---
+++ fractol/fractol.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpeixeir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 18:18:46 by mpeixeir          #+#    #+#             */
+/*   Updated: 2023/05/03 18:18:48 by mpeixeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+
+# define WINDOW_HEIGHT	1200
+# define WINDOW_WIDTH	1250
+# define MLX_ERROR		1
+
+#endif
